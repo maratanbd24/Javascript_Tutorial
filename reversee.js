a200f011507a83512d142861bfe88b7b851032de
m. a. ratan <maratanbd@gmail.com>
2024-03-05 04:38:50 +0600

    sami tumi koi

---
+++ reversee.js
@@ -0,0 +1,15 @@
+const numbers = [1,2,3,4,5,6,7,8,9]
+
+// console.log(numbers)
+
+// numbers.reverse();
+
+// console.log(numbers)
+
+const rev_numbers = [];
+for (const num of numbers){
+    console.log(num);
+    rev_numbers.push[num];
+
+}
+console.log(rev_numbers)
